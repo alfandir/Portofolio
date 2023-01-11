d27636704b90479cab304069288cc71de986d047
Alfandi <alfandisullivan@gmail.com>
2023-01-11 11:13:54 +0700

    first commit

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiLinux,
+  SiVisualstudiocode,
+  SiPostman,
+  SiHeroku,
+  SiVercel,
+  SiWindows,
+  SiFilezilla,
+  SiFigma,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiWindows />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFilezilla />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFigma />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
